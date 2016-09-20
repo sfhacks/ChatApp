91561a14f9adcc75a48ca81142a72641fcd4b983
Andrew Ke <andrew@andrewke.org>
2016-09-19 17:04:27 -0700

    Initial Commit

---
+++ Chat/ChatHelper.swift
@@ -0,0 +1,15 @@
+//
+//  ChatHelper.swift
+//  Chat
+//
+//  Created by Andrew Ke on 9/19/16.
+//  Copyright © 2016 sfhacks. All rights reserved.
+//
+
+import Foundation
+
+class ChatHelper {
+    static func sendMessage(message: String) {
+        // Send a message
+    }
+}
---
+++ Chat/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Chat
+//
+//  Copyright © 2016 sfhacks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // IB Outlet for message text box
+    
+    // IB Outlet for send button
+    
+
+}
+
